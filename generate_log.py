261a492ecb58e3dc22ecb82a20573c99815edd7f
Prathyusha-Guduru <prathyushaguduru28@gmail.com>
2022-01-24 18:56:51 +0530

    Updated 1000 rowed data

--- generate_log.py
+++ generate_log.py
@@ -9,14 +9,14 @@ import functools
 import os
 
 #Opening an empty text file and accessing write permission
-f= open("disk.txt","w")
 log={}
 
 no_of_racks = 10
 no_of_datanodes = 10
-no_of_files = 5
-no_of_blocks = 2
+no_of_files = 1000
+no_of_blocks = 100
 
+f= open("disk_log.txt","w")
 
 racks = []
 for i in range(0,no_of_racks):
@@ -29,6 +29,7 @@ for i in range(0, len(racks)):
 	for j in range(0,no_of_datanodes):
 		rack[f"R{i}"].append(f"R{i} D{j}")
 	data_node.append(rack)
+
 
 
 
@@ -45,29 +46,35 @@ for i in range(0,len(files)):
 		blocks[f"F{i}"].append(f"F{i}B{j}")
 	file_blocks.append(blocks)
 
+# print(f"Type of each element in file_blocks is {type(file_blocks[0])}")
+# print(type(list(file_blocks[0].values())))
 
 
 for i in file_blocks:
-	for j in i.values():
-		random_rack = random.sample(data_node,1)
-		random_node = random.sample(list(random_rack[-1].values())[-1],1)
-		print(random_node)
+	print(type(i))
+	for j in list(i.values()):
+		print(type(j))
+		for k in j:
+			print(type(k))
+			racks = random.sample(data_node,2)
+			print(racks)
+			first_rack = racks[0]
+			
+			nodes = random.sample(list(first_rack.values())[-1],2)
+			first_node = nodes[0]
+			second_node = nodes[1]
+			# print(first_rack[-1].keys())
+			# list(first_rack[-1].values())[-1].remove(first_node[-1])
+			# second_node = nodes[1]
+			# z = data_node
+			# z.remove(first_rack[-1])
+			# third_rack = random.sample(z,1)
+			second_rack = racks[1]
+			third_node = random.sample(list(second_rack.values())[-1],1)
+			s = f"{j}  {k} {first_node} {second_node} {third_node}\n"
+			f.write(s)
 
-		second_node = random.sample(list(random_rack[-1].values())[-1],1)
-		if(second_node != random_node):
-			print(f"second node {second_node}")
-		else:
-			second_node = random.sample(list(random_rack[-1].values())[-1],1)
-			print(f"second node {second_node}")
-		
-		
-		z = data_node
-		z.remove(random_rack[-1])
-		third_rack = random.sample(z,1)
-		third_node = random.sample(list(third_rack[-1].values())[-1],1)
-		print(third_node)
-		# print(list(random_rack[-1].values())[-1])
+f.close()
 
 
-print(f"\n\n\n{data_node}")
 
